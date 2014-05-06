b5d1bd3dc0c67de067c63286973d1cf16afbd7e9
Nick Greenfield <nick.greenfield@gmail.com>
2014-05-05 17:43:29 -0700

    Another messy interim checkin. Includes patches memfiles.nim module.

--- bitarray.nim
+++ bitarray.nim
@@ -1,6 +1,9 @@
-# from memfiles import nil
+#from memfiles import nil  # Not in love with this as it doesn't jive with method syntax, i.e., need to call memfiles.close(mm) vs. mm.close()
+import private/memfiles
+from os import nil
 from strutils import `%`
 import unsigned
+import baseutils  # Pointer arithmetic
 
 # Type declarations
 type
@@ -8,19 +11,48 @@ type
 
 type
   EBitarray = object of EBase
+  TBitarrayKind = enum inmem, mmap
+  PBitarray = ref TBitarray
   TBitarray = object
-    bitarray: seq[TBitScalar]
     size_elements: int
     size_bits: int
     size_specified: int
+    case in_memory: bool
+    of true:
+      bitarray: seq[TBitScalar]
+    of false:
+      bitarray_mmap: TMemFile
 
 
 proc create_bitarray(size: int): TBitarray =
-  ## Creates a bitarray using a specified input size.
-  ## Note that this will round up to the nearest byte
+  ## Creates an in-memory bitarray using a specified input size.
+  ## Note that this will round up to the nearest byte.
   let n_elements = size div (sizeof(TBitScalar) * 8)
   let n_bits = n_elements * (sizeof(TBitScalar) * 8)
-  result = TBitarray(bitarray: newSeq[TBitScalar](n_elements),
+  result = TBitarray(in_memory: true, bitarray: newSeq[TBitScalar](n_elements),
+                     size_elements: n_elements, size_bits: n_bits,
+                     size_specified: size)
+
+
+proc create_bitarray(file: string, size: int = -1): TBitarray =
+  ## Creates an mmap-backed bitarray. If the specified file exists
+  ## it will be opened, but an exception will be raised if the size
+  ## is specified and does not match. If the file does not exist
+  ## it will be created.
+  let n_elements = size div (sizeof(char) * 8)
+  let n_bits = n_elements * (sizeof(char) * 8)
+  var mm_file: TMemFile
+  if os.existsFile(file):
+    mm_file = open(file, mode = fmReadWrite, mappedSize = -1)
+    if size != -1 and mm_file.size != n_elements:
+      raise newException(EBitarray, "Existing mmap file does not have the specified size $1" % $size)
+  else:
+    if size == -1:
+      raise newException(EBitarray, "No existing mmap file. Must specify size to create one.")
+    mm_file = open(file, mode = fmReadWrite, newFileSize = n_elements)
+
+
+  result = TBitarray(in_memory: false, bitarray_mmap: mm_file,
                      size_elements: n_elements, size_bits: n_bits,
                      size_specified: size)
 
@@ -29,24 +61,25 @@ proc `[]=`*(ba: var TBitarray, index: int, val: bool) {.inline.} =
   ## Sets the bit at an index to be either 0 (false) or 1 (true)
   let i_element = index div (sizeof(TBitScalar) * 8)
   let i_offset = index mod (sizeof(TBitScalar) * 8)
-  # echo("Inserting $1 at position $2." % (index, int(val)))
-  if val:
-    ba.bitarray[i_element] = (ba.bitarray[i_element] or (0b1 shl i_offset))
-  else:
-    ba.bitarray[i_element] = (ba.bitarray[i_element] and ((not 0b1) shl i_offset))
+  if ba.in_memory:
+    if val:
+      ba.bitarray[i_element] = (ba.bitarray[i_element] or (0b1 shl i_offset))
+    else:
+      ba.bitarray[i_element] = (ba.bitarray[i_element] and ((not 0b1) shl i_offset))
 
 
 proc `[]`*(ba: var TBitarray, index: int): bool {.inline.} =
   ## Gets the bit at an index element (returns a bool)
   let i_element = index div (sizeof(TBitScalar) * 8)
   let i_offset = index mod (sizeof(TBitScalar) * 8)
-  result = bool((ba.bitarray[i_element] shr i_offset) and 1)
+  if ba.in_memory:
+    result = bool((ba.bitarray[i_element] shr i_offset) and 1)
 
 
 proc `$`(ba: TBitarray): string =
   ## Print the number of bits and elements in the bitarray (elements are currently defined as 8-bit chars)
-  result = ("Bitarray with $1 bits and $2 unique elements." %
-            [$ba.size_bits, $ba.size_elements])
+  result = ("Bitarray with $1 bits and $2 unique elements. In-memory?: $3." %
+            [$ba.size_bits, $ba.size_elements, $ba.in_memory])
 
 
 when isMainModule:
@@ -54,6 +87,7 @@ when isMainModule:
   var bitarray = create_bitarray(int(1e8))
   echo "Created a bitarray."
   echo bitarray
+  # echo type(bitarray)
   bitarray[0] = true
   echo bitarray.bitarray[0..10]
   bitarray[1] = true
@@ -70,14 +104,51 @@ when isMainModule:
   echo 0b10000000
   echo 0b11111111
 
-  type
-    TCharArray = array[0..5, char]
+  var bitarray_b = create_bitarray("/tmp/ba.mmap", size=1024 * 8)
+  echo bitarray_b
+  # echo bitarray_b.bitarray_mmap.mem.addr
+  echo "Size is ", sizeOf(bitarray_b.bitarray_mmap.mem)
+  # echo type(bitarray_b.bitarray_mmap.mem)
+  # echo bitarray_b.bitarray_mmap.mem[]
+  # echo cast[uint64](bitarray_b.bitarray_mmap.mem[])
+  echo cast[uint64](bitarray_b.bitarray_mmap.mem)
+  echo cast[uint64](bitarray_b.bitarray_mmap.mem)
+  echo cast[uint64](bitarray_b.bitarray_mmap.mem)
+  echo cast[uint64](bitarray_b.bitarray_mmap.mem)
+  echo cast[uint64](bitarray_b.bitarray_mmap.mem + 8)
 
-  var tc: TCharArray
-  tc = ['A', 'B', 'C', 'D', 'E', 'F']
-  echo tc
+  var
+    mm, mm_full: TMemFile
 
-  echo bitarray[0]
-  echo bitarray[1]
-  echo bitarray[2]
-  echo bitarray[3]
+  if not os.existsFile("/tmp/test.mmap"):
+    mm = memfiles.open("/tmp/test.mmap", mode = fmReadWrite, newFileSize = 1024)    # Create a new file
+    mm.close()
+
+  mm_full = memfiles.open("/tmp/test.mmap", mode = fmRead, mappedSize = -1)
+  mm_full.close()
+
+
+  var
+    tststr: string = "hello, world!"
+    tstptr: ptr string = addr(tststr)
+    tstptr2: pointer
+    memad: int
+
+  var p = cast[ptr array[1024, int8]](bitarray_b.bitarray_mmap.mem)
+  echo "P0 is ", p[0]
+  p[0] = 1'i8
+  echo "P0 is ", p[0]
+
+  # echo bitarray_b.bitarray_mmap.mem[]
+
+  echo (type(tstptr) is ptr)
+  echo (type(bitarray_b.bitarray_mmap.mem) is ptr)
+  echo (type(bitarray_b.bitarray_mmap.mem) is pointer)
+  # echo type(p)
+  echo cast[ptr int](bitarray_b.bitarray_mmap.mem + 8)[]
+  echo tstptr[] # prints hello, world!
+  memad = cast[int](tstptr) # address, same as the one listed in repr(tstptr)!
+  echo memad
+  tstptr2 = cast[pointer](memad) # generic pointer to address?
+  tstptr = cast[ptr string](tstptr2) # converting back to pointer to string?
+  echo tstptr[] # SIGSEV :-(
---
+++ private/memfiles.nim
@@ -0,0 +1,247 @@
+#
+#
+#            Nimrod's Runtime Library
+#        (c) Copyright 2014 Nimrod Contributors
+#
+#    See the file "copying.txt", included in this
+#    distribution, for details about the copyright.
+#
+
+## :Authors: Zahary Karadjov, Andreas Rumpf
+##
+## This module provides support for `memory mapped files`:idx:
+## (Posix's `mmap`:idx:) on the different operating systems.
+
+when defined(windows):
+  import winlean
+elif defined(posix):
+  import posix
+else:
+  {.error: "the memfiles module is not supported on your operating system!".}
+
+import os
+
+type
+  TMemFile* = object {.pure.} ## represents a memory mapped file
+    mem*: pointer    ## a pointer to the memory mapped file. The pointer
+                     ## can be used directly to change the contents of the
+                     ## file, if it was opened with write access.
+    size*: int       ## size of the memory mapped file
+
+    when defined(windows):
+      fHandle: int
+      mapHandle: int
+    else:
+      handle: cint
+
+
+proc mapMem*(m: var TMemFile, mode: TFileMode = fmRead,
+             mappedSize = -1, offset = 0): pointer =
+  var readonly = mode == fmRead
+  when defined(windows):
+    result = mapViewOfFileEx(
+      m.mapHandle,
+      if readonly: FILE_MAP_READ else: FILE_MAP_WRITE,
+      int32(offset shr 32),
+      int32(offset and 0xffffffff),
+      if mappedSize == -1: 0 else: mappedSize,
+      nil)
+    if result == nil:
+      osError(osLastError())
+  else:
+    assert mappedSize > 0
+    result = mmap(
+      nil,
+      mappedSize,
+      if readonly: PROT_READ else: PROT_READ or PROT_WRITE,
+      if readonly: MAP_PRIVATE else: MAP_SHARED,
+      m.handle, offset)
+    if result == cast[pointer](MAP_FAILED):
+      osError(osLastError())
+
+
+proc unmapMem*(f: var TMemFile, p: pointer, size: int) =
+  ## unmaps the memory region ``(p, <p+size)`` of the mapped file `f`.
+  ## All changes are written back to the file system, if `f` was opened
+  ## with write access. ``size`` must be of exactly the size that was requested
+  ## via ``mapMem``.
+  when defined(windows):
+    if unmapViewOfFile(p) == 0: osError(osLastError())
+  else:
+    if munmap(p, size) != 0: osError(osLastError())
+
+
+proc open*(filename: string, mode: TFileMode = fmRead,
+           mappedSize = -1, offset = 0, newFileSize = -1): TMemFile =
+  ## opens a memory mapped file. If this fails, ``EOS`` is raised.
+  ## `newFileSize` can only be set if the file does not exist and is opened
+  ## with write access (e.g., with fmReadWrite). `mappedSize` and `offset`
+  ## can be used to map only a slice of the file. Example:
+  ##
+  ## .. code-block:: nimrod
+  ##   var
+  ##     mm, mm_full, mm_half: TMemFile
+  ##
+  ##   mm = memfiles.open("/tmp/test.mmap", mode = fmWrite, newFileSize = 1024)    # Create a new file
+  ##   mm.close()
+  ##
+  ##   # Read the whole file, would fail if newFileSize was set
+  ##   mm_full = memfiles.open("/tmp/test.mmap", mode = fmReadWrite, mappedSize = -1)
+  ##
+  ##   # Read the first 512 bytes
+  ##   mm_half = memfiles.open("/tmp/test.mmap", mode = fmReadWrite, mappedSize = 512)
+
+  # The file can be resized only when write mode is used:
+  assert newFileSize == -1 or mode != fmRead
+  var readonly = mode == fmRead
+
+  template rollback =
+    result.mem = nil
+    result.size = 0
+
+  when defined(windows):
+    template fail(errCode: TOSErrorCode, msg: expr) =
+      rollback()
+      if result.fHandle != 0: discard closeHandle(result.fHandle)
+      if result.mapHandle != 0: discard closeHandle(result.mapHandle)
+      osError(errCode)
+      # return false
+      #raise newException(EIO, msg)
+
+    template callCreateFile(winApiProc, filename: expr): expr =
+      winApiProc(
+        filename,
+        if readonly: GENERIC_READ else: GENERIC_ALL,
+        FILE_SHARE_READ,
+        nil,
+        if newFileSize != -1: CREATE_ALWAYS else: OPEN_EXISTING,
+        if readonly: FILE_ATTRIBUTE_READONLY else: FILE_ATTRIBUTE_TEMPORARY,
+        0)
+
+    when useWinUnicode:
+      result.fHandle = callCreateFile(createFileW, newWideCString(filename))
+    else:
+      result.fHandle = callCreateFile(createFileA, filename)
+
+    if result.fHandle == INVALID_HANDLE_VALUE:
+      fail(osLastError(), "error opening file")
+
+    if newFileSize != -1:
+      var
+        sizeHigh = int32(newFileSize shr 32)
+        sizeLow  = int32(newFileSize and 0xffffffff)
+
+      var status = setFilePointer(result.fHandle, sizeLow, addr(sizeHigh),
+                                  FILE_BEGIN)
+      let lastErr = osLastError()
+      if (status == INVALID_SET_FILE_POINTER and lastErr.int32 != NO_ERROR) or
+         (setEndOfFile(result.fHandle) == 0):
+        fail(lastErr, "error setting file size")
+
+    # since the strings are always 'nil', we simply always call
+    # CreateFileMappingW which should be slightly faster anyway:
+    result.mapHandle = createFileMappingW(
+      result.fHandle, nil,
+      if readonly: PAGE_READONLY else: PAGE_READWRITE,
+      0, 0, nil)
+
+    if result.mapHandle == 0:
+      fail(osLastError(), "error creating mapping")
+
+    result.mem = mapViewOfFileEx(
+      result.mapHandle,
+      if readonly: FILE_MAP_READ else: FILE_MAP_WRITE,
+      int32(offset shr 32),
+      int32(offset and 0xffffffff),
+      if mappedSize == -1: 0 else: mappedSize,
+      nil)
+
+    if result.mem == nil:
+      fail(osLastError(), "error mapping view")
+
+    var hi, low: int32
+    low = getFileSize(result.fHandle, addr(hi))
+    if low == INVALID_FILE_SIZE:
+      fail(osLastError(), "error getting file size")
+    else:
+      var fileSize = (int64(hi) shr 32) or low
+      if mappedSize != -1: result.size = min(fileSize, mappedSize).int
+      else: result.size = fileSize.int
+
+  else:
+    template fail(errCode: TOSErrorCode, msg: expr) =
+      rollback()
+      if result.handle != 0: discard close(result.handle)
+      osError(errCode)
+
+    var flags = if readonly: O_RDONLY else: O_RDWR
+
+    if newFileSize != -1:
+      flags = flags or O_CREAT or O_TRUNC
+      # Grant user r/w permission for new files
+      var permissions_mode = S_IRUSR or S_IWUSR
+      result.handle = open(filename, flags, permissions_mode)
+    else:
+      result.handle = open(filename, flags)
+
+    if result.handle == -1:
+      # XXX: errno is supposed to be set here
+      # Is there an exception that wraps it?
+      fail(osLastError(), "error opening file")
+
+    if newFileSize != -1:
+      if ftruncate(result.handle, newFileSize) == -1:
+        fail(osLastError(), "error setting file size")
+
+    if mappedSize != -1:
+      result.size = mappedSize
+    else:
+      var stat: TStat
+      if fstat(result.handle, stat) != -1:
+        # XXX: Hmm, this could be unsafe
+        # Why is mmap taking int anyway?
+        result.size = int(stat.st_size)
+      else:
+        fail(osLastError(), "error getting file size")
+
+    result.mem = mmap(
+      nil,
+      result.size,
+      if readonly: PROT_READ else: PROT_READ or PROT_WRITE,
+      if readonly: MAP_PRIVATE else: MAP_SHARED,
+      result.handle,
+      offset)
+
+    if result.mem == cast[pointer](MAP_FAILED):
+      fail(osLastError(), "file mapping failed")
+
+proc close*(f: var TMemFile) =
+  ## closes the memory mapped file `f`. All changes are written back to the
+  ## file system, if `f` was opened with write access.
+
+  var error = false
+  var lastErr: TOSErrorCode
+
+  when defined(windows):
+    if f.fHandle != INVALID_HANDLE_VALUE:
+      error = unmapViewOfFile(f.mem) == 0
+      lastErr = osLastError()
+      error = (closeHandle(f.mapHandle) == 0) or error
+      error = (closeHandle(f.fHandle) == 0) or error
+  else:
+    if f.handle != 0:
+      error = munmap(f.mem, f.size) != 0
+      lastErr = osLastError()
+      error = (close(f.handle) != 0) or error
+
+  f.size = 0
+  f.mem = nil
+
+  when defined(windows):
+    f.fHandle = 0
+    f.mapHandle = 0
+  else:
+    f.handle = 0
+
+  if error: osError(lastErr)
+
