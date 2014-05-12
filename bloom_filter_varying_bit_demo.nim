a2102e2308c1719298cc1416fba187c140993d8e
Nick Greenfield <nick.greenfield@gmail.com>
2014-05-12 16:53:01 -0700

    Bloom filter varying bits demo.

---
+++ bloom_filter_varying_bit_demo.nim
@@ -0,0 +1,79 @@
+import bitarray
+import murmur3
+import strutils
+import times
+from math import random, randomize
+import unsigned
+
+
+type
+  TBloomFilter = object
+    bitarray: TBitarray
+    n_hashes: int
+    n_bits_per_item: int
+    n_bits: int
+
+proc create_bloom_filter*(n_elements: int, n_bits_per_item: int = 12, n_hashes: int = 6): TBloomFilter =
+  ## Generate a Bloom filter, yay so simple!
+  let n_bits = n_elements * n_bits_per_item
+  result = TBloomFilter(
+      bitarray: create_bitarray(n_bits),
+      n_hashes: n_hashes,
+      n_bits_per_item: n_bits_per_item,
+      n_bits: n_bits
+    )
+
+{.push overflowChecks: off.}
+proc hash(bf: TBloomFilter, item: string): seq[int] =
+  var hashes: TMurmurHashes = murmur_hash(item, 0)
+  newSeq(result, bf.n_hashes)
+  for i in 0..(bf.n_hashes - 1):
+    result[i] = abs(hashes[0] + hashes[1] * i) mod (bf.n_bits - (sizeof(TBitScalar) * 8))
+  return result
+{.pop.}
+
+proc insert*(bf: var TBloomFilter, item: string) =
+  ## Put the string there
+  let hashes = hash(bf, item)
+  let insert_pattern: TBitScalar = TBitScalar(1) shl TBitScalar(hashes[0] mod (8 * sizeof(TBitScalar)))
+  for h in hashes:
+    bf.bitarray[h..(h + sizeof(TBitScalar) * 8)] = insert_pattern
+
+proc lookup*(bf: var TBloomFilter, item: string): bool =
+  ## Is the string there?
+  let hashes = hash(bf, item)
+  let pattern = TBitScalar(1) shl TBitScalar(hashes[0] mod (8 * sizeof(TBitScalar)))
+  var lookup: TBitScalar = pattern
+  for h in hashes:
+    lookup = lookup and bf.bitarray[h..(h + sizeof(TBitScalar) * 8)]
+  result = (lookup == pattern)
+
+
+when isMainModule:
+  echo "Quick working Bloom filter example."
+  let n_tests = int(1e6)
+  var bf = create_bloom_filter(n_elements = n_tests, n_bits_per_item = 12, n_hashes = 7)
+  bf.insert("Here we go!")
+  assert bf.lookup("Here we go!")
+  assert (not bf.lookup("I'm not here."))
+
+  let test_string_len = 50
+  let sample_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  var k_test_elements = newSeq[string](n_tests)
+  for i in 0..(n_tests - 1):
+    var new_string = ""
+    for j in 0..(test_string_len):
+      new_string.add(sample_chars[random(51)])
+    k_test_elements[i] = new_string
+
+  let start_time = cpuTime()
+  for i in 0..(n_tests - 1):
+    bf.insert(k_test_elements[i])
+  let end_time = cpuTime()
+  echo("Took ", formatFloat(end_time - start_time, format = ffDecimal, precision = 4), " seconds to insert ", n_tests, " items.")
+
+  let start_time_b = cpuTime()
+  for i in 0..(n_tests - 1):
+    doAssert bf.lookup(k_test_elements[i])
+  let end_time_b = cpuTime()
+  echo("Took ", formatFloat(end_time_b - start_time_b, format = ffDecimal, precision = 4), " seconds to lookup ", n_tests, " items.")
